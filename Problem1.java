2e6958bee353e57d96337966dabe0eec3b4c3c5d
Yash Thakur <144375330+thakuryash060@users.noreply.github.com>
2025-07-21 20:44:00 +0530

    Create Problem1.java
    TC : O(n)
    SC : O(h)

---
+++ Problem1.java
@@ -0,0 +1,25 @@
+class BSTIterator {
+    Stack<TreeNode>s;
+    public BSTIterator(TreeNode root) {
+        s = new Stack<>();
+        dfs(root);
+    }
+    
+    public int next() {
+        TreeNode popped = s.pop();
+        dfs(popped.right);
+        return popped.val;
+    }
+    
+    public boolean hasNext() {
+        return !s.isEmpty();
+    }
+
+    private void dfs(TreeNode root){
+        while(root!=null){
+            s.add(root);
+            root=root.left;
+        }
+    }
+
+}
